799a4e3017f63b5080f561d008d533f269e27158
alex-laycalvert <alex.laycalvert@gmail.com>
2021-12-24 20:29:00 -0500

    Changed some colors and added a qtile keymap

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -32,8 +32,15 @@ from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 
 mod = "mod4"
+alt = "mod1"
+shift = "shift"
+space = "space"
+ctrl = "control"
+enter = "Return"
+tab = "Tab"
 
 terminal = "kitty"
+browser = "brave"
 
 keys = [
     # Switch between windows
@@ -41,46 +48,39 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(),
-        desc="Move window focus to other window"),
+    Key([mod], space, lazy.layout.next(), desc="Move window focus to other window"),
 
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
-        desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
-        desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
-        desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod, shift], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, shift], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, shift], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, shift], "k", lazy.layout.shuffle_up(), desc="Move window up"),
 
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(),
-        desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(),
-        desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(),
-        desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod, ctrl], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, ctrl], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, ctrl], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, ctrl], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
 
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack"),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([mod, shift], enter, lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack"),
+    Key([mod], enter, lazy.spawn(terminal), desc="Launch terminal"),
+    Key([mod], "b", lazy.spawn(browser), desc="Spawn Browser"),
 
     # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], tab, lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
 
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(),
-        desc="Spawn a command using a prompt widget"),
+    Key([mod, ctrl], "r", lazy.restart(), desc="Restart Qtile"),
+    Key([mod, ctrl], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([alt], space, lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 
 ]
 
