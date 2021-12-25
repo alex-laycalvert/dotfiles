7871b8ef0f77984165f7e6102c79dd4e54831dfe
alex-laycalvert <alex.laycalvert@gmail.com>
2021-12-24 19:50:20 -0500

    Added some more things I left out last commit

--- scripts/kitty-theme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# change-kitty-theme.sh
-
-KITTY_THEME=`/usr/bin/ls $HOME/.config/kitty/themes/ | dmenu -p "Kitty Theme: "`
-sed "s/include themes\/.*[.]conf/include themes\/$KITTY_THEME/" $HOME/.config/kitty/kitty.conf > kitty.conf.new
-mv kitty.conf.new $HOME/.config/kitty/kitty.conf
-kill -USR1 $KITTY_PID
