c0e0f1ef5c58f70047b44cb0a4b4b1081824cf08
Alexander Lay-Calvert <alex.laycalvert@gmail.com>
2022-09-18 21:05:22 -0400

    Changed neovim LSP and completion engine, added some snippets

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -3,11 +3,100 @@
 -- 
 -- https://github.com/alex-laycalvert
 
-vim.cmd([[let g:coq_settings = { 'auto_start': 'shut-up', 'keymap.jump_to_mark': '<M-n>' }]])
+--------------------------
+--- SETUP FOR cmp-nvim ---
+--------------------------
+local cmp = require('cmp')
 
-local lsp = require('lspconfig')
-local coq = require('coq')
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
-lsp.tsserver.setup(coq.lsp_ensure_capabilities())
+local feedkey = function(key, mode)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
 
-vim.cmd('COQsnips compile')
+vim.cmd('set completeopt=menu,menuone,noselect')
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            vim.fn['vsnip#anonymous'](args.body) -- vsnip
+            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered()
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<M-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+              cmp.select_next_item()
+          elseif has_words_before() then
+              cmp.complete()
+          else
+              fallback()
+          end
+      end, { 'i', 's' }),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+              cmp.select_prev_item()
+          end
+      end, { 'i', 's' }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' },
+        { name = 'cmp_tabnine' }
+    }, {
+        { name = 'buffer' }
+    })
+})
+
+vim.cmd([[
+    imap <expr> <M-n>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+    smap <expr> <M-n>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+    imap <expr> <S-M-n> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+    smap <expr> <S-M-n> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+]])
+
+cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+        { name = 'cmp_git' },
+    }, {
+        { name = 'buffer' }
+    })
+})
+
+cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    })
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig').tsserver.setup {
+    capabilities = capabilities
+}
--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -10,25 +10,65 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use 'neovim/nvim-lspconfig'
-    use 'dracula/vim'
+    -- plugin manager
+    use { 'wbthomason/packer.nvim' }
+
+    -- lsp
+    use { 'neovim/nvim-lspconfig' }
+    use { 'hrsh7th/cmp-nvim-lsp' }
+    use { 'hrsh7th/cmp-buffer' }
+    use { 'hrsh7th/cmp-path' }
+    use { 'hrsh7th/cmp-cmdline' }
+    use { 'hrsh7th/nvim-cmp' }
+    use { 'tzachar/cmp-tabnine', run = './install.sh', requires = 'hrsh7th/nvim-cmp' }
+
+    -- snippets
+    use { 'hrsh7th/cmp-vsnip' }
+    use { 'hrsh7th/vim-vsnip' }
+
+    -- -- luasnips
+    -- use { 'L3MON4D3/LuaSnip' }
+    -- use { 'saadparwaiz1/cmp_luasnip' }
+
+    -- -- ultisnips
+    -- use { 'SirVer/ultisnips' }
+    -- use { 'quangnguyen30192/cmp-nvim-ultisnips' }
+
+    -- -- snippy
+    -- use { 'dcampos/nvim-snippy' }
+    -- use { 'dcampos/cmp-snippy' }
+
+    -- colortheme
+    use { 'dracula/vim' }
+
+    -- filetree
     use { 'ms-jpq/chadtree', branch = 'chad', run = 'python3 -m chadtree deps' }
-    use { 'ms-jpq/coq_nvim', branch = 'coq' }
-    use { 'ms-jpq/coq.artifacts', branch = 'artifacts' }
-    use { 'ms-jpq/coq.thirdparty', branch = '3p' }
+    -- use { 'ms-jpq/coq_nvim', branch = 'coq' }
+    -- use { 'ms-jpq/coq.artifacts', branch = 'artifacts' }
+    -- use { 'ms-jpq/coq.thirdparty', branch = '3p' }
+
+    -- commentor
     use { 'terrortylor/nvim-comment' }
+
+    -- status line
     use {
         'nvim-lualine/lualine.nvim',
         requires = {'kyazdani42/nvim-web-devicons', opt = true}
     }
+
+    -- icons
     use { 'ryanoasis/vim-devicons' }
+
+    -- lazygit
     use { 'kdheepak/lazygit.nvim' }
-    use { 'jiangmiao/auto-pairs' }
+
+    -- fuzzy finding of files and buffers
     use {
         'nvim-telescope/telescope.nvim',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
+
+    -- auto docs
     use { 'kkoomen/vim-doge' }
 end)
 
@@ -37,5 +77,3 @@ require('nvim_comment').setup({
     operator_mapping = '<M-/>',
 })
 require('lualine').setup()
-
-vim.g.AutoPairsShortcutJump = '<M-b>'
